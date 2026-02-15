3302846b8558a1a7a77922dc32c95443e26d7138
waleed truman <waledmet@gmail.com>
2026-02-15 16:55:27 +0300

    My site part 1

--- mysite/core/urls.py
+++ mysite/core/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from core import views
 
 urlpatterns = [
-    path('', include('core.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('txt', views.index, name='index')
 ]
